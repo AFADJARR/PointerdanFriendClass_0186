672f814bf593d79d56899f8189b62a306d77628b
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-05 16:02:07 +0700

    hasil folder friend class

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -3,12 +3,46 @@
 using namespace std;
 
 class siswa;
-class orang;
+class orang
 {
-privat:
+private:
     string nama;
 
-publicL:
+public:
     void setNama(string pNama);
-    Friend Class siswa;
+    friend class siswa;
 };
+
+class siswa
+{
+private:
+    int id;\
+
+public:
+    void setId(int pId);
+    void displayALL(orang &a);
+};
+
+void orang:: setNama(string pNama)
+{
+    nama = pNama;
+}
+ void siswa::setId(int pId)
+ {
+    id= pId;
+ }
+
+ void siswa::displayALL(orang&a)
+ {
+    cout << id << endl << a.nama;
+ }
+
+ int main()
+ {
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayALL(joko);
+    return 0;
+ }
