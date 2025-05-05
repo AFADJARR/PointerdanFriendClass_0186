dc86a0b7b46956199ed97c14de38a6aa23c31104
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-05 15:47:20 +0700

    menambahkan class dan memberi fungssi friend class

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class siswa;
+class orang;
+{
+privat:
+    string nama;
+
+publicL:
+    void setNama(string pNama);
+    Friend Class siswa;
+};
