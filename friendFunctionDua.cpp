2d2b9e440ea72a32ffa4929937f8bdb95e768355
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-05-05 16:11:33 +0700

    membuat showNilaiPelajar

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class pelajar;
+class manusia
+{
+public:
+    void showNilaiPelajar(pelajar &x);
+    void showNilaiPelajarDua(pelajar &y);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    
+}
